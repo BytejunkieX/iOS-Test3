a270ee9de80df933f68fa2346a473de94aeab57c
BytejunkieX <mike.quade@me.com>
2013-07-27 12:26:18 +0200

    Zurück

--- iOS-Test2/AppDelegate.m
+++ iOS-Test2/AppDelegate.m
@@ -16,7 +16,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    NSLog(@"Hallo Welt 2");
+    NSLog(@"Hallo Welt");
     return YES;
 }
 
