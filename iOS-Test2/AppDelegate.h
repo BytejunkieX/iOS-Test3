9481a6de84f2e0a92f2d9d1c629520b49366e8c0
Mike Quade <mike@macmini.localdomain>
2013-07-27 11:40:40 +0200

    Initial Commit

---
+++ iOS-Test2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Test2
+//
+//  Created by Mike Quade on 27.07.13.
+//  Copyright (c) 2013 Mike Quade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
